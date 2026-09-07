9ef0dc5967a1c4d5187b11fe93dfc4478b389f03
Xyrex Kyle Salazar <122286060+xyxyxyrex@users.noreply.github.com>
2026-09-07 22:28:45 +0800

    Remove obsolete lobby DOM helper

--- src/menu.js
+++ src/menu.js
@@ -1,28 +1,20 @@
-
 function startGame() {
-    document.getElementById("c").style.display = ""
-    document.getElementById("menu").style.display = "none"
-    document.getElementById("restartLevel").style.display = ""
+    document.getElementById("c").style.display = "";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("restartLevel").style.display = "";
     if (!mainGame.matter.running) {
-        startMainGame()
+        startMainGame();
         setInterval(() => {
-            if (window.hostConnection) hostConnection.broadcast(JSON.stringify({
-                startGame:true,
-            }))
+            if (window.hostConnection) hostConnection.broadcast(JSON.stringify({startGame:true}));
         }, 3000);
     }
-
 }
-function hideGame() {
-    document.getElementById("menu").style.display = ""
-    document.getElementById("c").style.display = "none"
 
+function hideGame() {
+    document.getElementById("menu").style.display = "";
+    document.getElementById("c").style.display = "none";
 }
 
 function setRoomCode(c) {
-    document.getElementById("roomCode").textContent = c
-}
-
-function addPlayerToMenu(name) {
-    document.getElementById("memberlist").appendChild(createElementFromHTML(`<div>${name}<br></div>`))
+    document.getElementById("roomCode").textContent = c;
 }
