28fed681ed9c19522288e5b9248995c34ffc9897
Xyrex Kyle Salazar <122286060+xyxyxyrex@users.noreply.github.com>
2026-09-12 11:28:51 +0800

    Add WASD aliases for primary player controls

--- src/controls.js
+++ src/controls.js
@@ -1,16 +1,42 @@
 var keys = {},
-  preKeys = {};
+  preKeys = {},
+  rawKeys = {},
+  wasdAliasesEnabled = true;
+
+const wasdAliases = {
+  a: "arrowleft",
+  d: "arrowright",
+  w: "arrowup",
+  s: "arrowdown",
+};
+
+function refreshKeys() {
+  keys = { ...rawKeys };
+  if (wasdAliasesEnabled)
+    Object.entries(wasdAliases).forEach(([wasd, arrow]) => {
+      if (rawKeys[wasd]) keys[arrow] = true;
+    });
+}
+
+function setWasdAliasesEnabled(enabled) {
+  wasdAliasesEnabled = !!enabled;
+  refreshKeys();
+}
+
 document.addEventListener("keydown", (e) => {
   if (e.code == "F1") mainGame.renderer.debug = !mainGame.renderer.debug;
-  keys[e.key.toLowerCase()] = true;
+  const key = e.key.toLowerCase();
+  rawKeys[key] = true;
+  refreshKeys();
   if (window.clientConnection && !e.repeat) {
-    if (e.key.toLowerCase() === "arrowup")
+    if (key === "arrowup" || (wasdAliasesEnabled && key === "w"))
       clientConnection.jumpSequence = (clientConnection.jumpSequence || 0) + 1;
     clientConnection.updateHost();
   }
 });
 document.addEventListener("keyup", (e) => {
-  keys[e.key.toLowerCase()] = false;
+  rawKeys[e.key.toLowerCase()] = false;
+  refreshKeys();
   if (window.clientConnection) clientConnection.updateHost();
 });
 
@@ -18,6 +44,7 @@ function updateControls() {
   preKeys = { ...keys };
 }
 window.addEventListener("blur", () => {
+  rawKeys = {};
   keys = {};
   preKeys = {};
   if (window.clientConnection) clientConnection.updateHost();
